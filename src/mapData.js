385393c3dd4297b110ada28b97115b9c9a16cae2
steve <steven.kerrick@gmail.com>
2022-07-13 19:30:48 -0500

    convert to webpack build

---
+++ src/mapData.js
@@ -0,0 +1,75 @@
+export default {
+  locations: [
+    {
+      name: "Acacia",
+      type: "home",
+      nether: [-160, 59, -168],
+    },
+    {
+      name: "Jungle",
+      type: "portal",
+      nether: [-347, 62, -333],
+    },
+    {
+      name: "Sunflower",
+      type: "portal",
+      nether: [-360, 64, -656],
+    },
+    {
+      name: "Flower Mine",
+      type: "portal",
+      nether: [-611, 62, -339],
+    },
+    {
+      name: "Ice Spikes",
+      type: "portal",
+      nether: [-613, 73, 340],
+    },
+    {
+      name: "Casey's House",
+      type: "home",
+      nether: [38, 68, -39],
+    },
+    {
+      name: "Michelle's",
+      type: "portal",
+      nether: [-17, 60, -168],
+    },
+    {
+      name: "Dorginton",
+      type: "portal",
+      nether: [-299, 90, -234],
+    },
+    {
+      name: "Two Castles",
+      type: "portal",
+      nether: [-160, 59, 953],
+    },
+    {
+      name: "Mansion 1",
+      type: "portal",
+      nether: [1204, 59, 953],
+    },
+  ],
+
+  paths: [
+    // Acacia to Jungle
+    [-160, -168, -160, -333, -347, -333],
+    // Jungle to Sunflower
+    [-347, -333, -347, -623, -360, -623, -360, -656],
+    // Jungle to Flower Mine
+    [-347, -337, -611, -337],
+    // Flower Mine to Ice Spikes
+    [-612, -339, -612, 340],
+    // Casey to Michelle
+    [38, -39, 38, -168, -17, -168],
+    // Michelle to Acacia
+    [-17, -168, -160, -168],
+    // Acacia to Dorginton
+    [-160, -168, -299, -168, -299, -234],
+    // Acacia to Two Castles
+    [-160, -168, -160, 953],
+    // Two Castles to Mansion 1
+    [-160, 953, 1204, 953],
+  ]
+}
